617089834df0552ba40f9bd617980b643f029209
Pranjal Butola <pranjalbutola12@gmail.com>
2024-04-17 13:33:25 +0530

    init vite and dark mode support

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { useDarkMode } from './hooks/use-dark-mode';
+
+export function App() {
+  useDarkMode();
+
+  return <div></div>;
+}
---
+++ src/hooks/use-dark-mode.ts
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+
+/**
+ * Defaults browser to dark mode by adding class="dark" to html element
+ * Have to do this as it doesnt work if u simply add class="dark" in index.html
+ */
+export function useDarkMode() {
+  useEffect(() => {
+    const isDarkMode = document.documentElement.classList.contains('dark');
+    console.log(isDarkMode);
+
+    if (isDarkMode) return;
+
+    document.documentElement.classList.add('dark');
+  }, []);
+}
